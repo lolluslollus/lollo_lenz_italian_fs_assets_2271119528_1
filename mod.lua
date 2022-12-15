1d380eb9e660164278bb265297df0888e3a5236b
lolluslollus <lolluslollus@users.noreply.github.com>
2022-12-15 19:50:07 +0100

    Initial commit

---
+++ mod.lua
@@ -0,0 +1,23 @@
+function data()
+	return {  
+		info = {
+			tags = { "FS", "Muro", "Italia", "Wall", "Retaining", "Muraglia", "Galleria", "Ferrovie dello Stato", "italian", "italy"},
+			minorVersion = 2,
+			severityAdd = "NONE",
+			severityRemove = "CRITICAL",
+			name = _("Italian FS Assets - SET Mura & Gallerie"),
+			description = _("Classiche mura di contenimento visibili lungo diverse ferrovie italiane, in aggiunta nel nuovo update 1.2 ci sono 4 nuovi modelli di 'salite' e 3 tipi di tunnel"),
+			authors = {
+				{
+					name = "Lenz",
+					role = "CREATOR",
+				},
+			},
+			visible = true,
+		},
+		options = {
+		},
+		runFn = function (settings)		
+		end
+	}
+end
